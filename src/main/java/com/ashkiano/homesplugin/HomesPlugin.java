05f749c71e8ada97f498a4da67c1cbd1a6e0e51d
Josef Vyskoèil <josef.vyskocil@cleverlance.com>
2024-03-13 16:18:31 +0100

    version 1.2

--- src/main/java/com/ashkiano/homesplugin/HomesPlugin.java
+++ src/main/java/com/ashkiano/homesplugin/HomesPlugin.java
@@ -17,6 +17,7 @@ public class HomesPlugin extends JavaPlugin {
         saveDefaultConfig();
 
         Metrics metrics = new Metrics(this, 21220);
+        this.getLogger().info("Thank you for using the HomesPlugin plugin! If you enjoy using this plugin, please consider making a donation to support the development. You can donate at: https://donate.ashkiano.com");
     }
 
     @Override
